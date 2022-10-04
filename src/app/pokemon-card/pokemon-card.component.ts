704d55f67d5c3382fef53b469b78648c664b33aa
André Luan de Oliveira <aluanbh@gmail.com>
2022-10-04 17:25:27 -0300

    primeiro commit

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass']
+})
+export class PokemonCardComponent {
+  @Input()
+  pokemon?: string;
+
+  @Input()
+  numero: number | undefined;
+
+  pegarImagemPokemon() {
+
+
+    const numeroFormatado = this.leadingZero(this.numero, 3);
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numeroFormatado}.png`;
+  }
+
+  leadingZero(str: string | number | undefined, size: 3): string {
+    let s = String(str);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+  }
+
+}
---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
+})
+export class PokemonListComponent {
+  pokemons = [
+    'Bulbasaur',
+    'Ivysaur',
+    'Venusaur',
+    'Charmander',
+    'Charmeleon',
+    'Charizard',
+    'Squirtle',
+    'Wartortle',
+  ];
+}
